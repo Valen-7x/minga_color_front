9a8f32ecca3d582bc7ee0113c84049d65a9fca6b
mariano5troncoso <mariano.nicolas.troncoso@gmail.com>
2023-07-08 14:15:18 -0300

    m02-rama

--- src/componentes/NotAllowed.jsx
+++ src/componentes/NotAllowed.jsx
@@ -6,15 +6,16 @@ export default function NotAllowed() {
   return (
     <div className='flex flex-col justify-center items-center  gap-10 w-full h-[87vh] bg-[white]'>
       <h1 className="hidden md:w-[100%] md:flex md:self-center font-bold md:text-6xl md:text-center md:justify-center relative z-10">
-        Tenes que estar Logueado para acceder 
+      There was an error loading the page
       </h1>
-<Anchor to="/signup"
+<Anchor to="/"
                 /*</div>onClick={handleFormSubmit}*/
                 className="w-[50vh] bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Crea una cuenta 
+                Go to Home
               </Anchor>
+  
     </div>
   );
 }
