daf589e0a3fb1339699911337a91105b2877d8be
mariano5troncoso <131175868+mariano5troncoso@users.noreply.github.com>
2023-07-09 12:37:26 -0300

    m02-rama (#10)

--- router/router.jsx
+++ router/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 
 import SignIn from "../src/componentes/SignIn";
 import SignUp from "../src/componentes/SignUp";
@@ -8,40 +8,49 @@ import NotAllowed from "../src/componentes/NotAllowed";
 import NotAllowedProtected from "./NotAllowedProtected";
 import ChapterForm from "../src/componentes/ChapterForm";
 import MangaForm from "../src/componentes/MangaForm";
+
+let token = localStorage.getItem('token');
+
+
 const router = createBrowserRouter([
-    {
-        path:"/",
-        element:<Main/>,
-        children:[            
-            {
-                path:'/',
-                element: <App/>
-            },
-            {
-                path: "/not-allowed",
-                element:  <NotAllowed/> ,
-              },
-              {
-                path:"/:manga_id/chapther-form",
-                element: <ChapterForm/>,
-              },
-              {
-                path:"/mangas",
-                element: <MangaForm/>
-              }
-        ]
-    },  
-          
-        {
-            path:'/signup',
-            element: <SignUp/>
-        },
-        {
-            path: "/SignIn",
-            element: <SignIn />,
-        
-          },
-         
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <App />
+      },
+      {
+        path: "/not-allowed",
+        element: <NotAllowed />,
+      },
+      {
+        path: "/:manga_id/chapther-form",
+        element: <ChapterForm />,
+      },
+      {
+        path: "/mangas",
+        element:  <MangaForm />
+      }
+    ]
+  },
+
+  {
+    path: '/signup',
+    element: !token ? <SignUp /> : <Navigate to="/not-allowed2" />
+  },
+  {
+    path: "/SignIn",
+    element: !token ? <SignIn /> : <Navigate to="/not-allowed2" />
+
+  },
+  {
+    path: "/not-allowed2",
+    element: <NotAllowed />,
+  },
+
+
 
 ])
 export default router
